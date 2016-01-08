6d65348a2acb6dc654155724dcaa573141e83c3a
mynameiscraziu <tarikia7@gmail.com>
2016-01-08 15:31:08 +0000

    Update bot.lua

--- bot/bot.lua
+++ bot/bot.lua
@@ -215,7 +215,7 @@ function create_config( )
       "moderation",
       "plugins",
       "version"},
-    sudo_users = {76534150},
+    sudo_users = {156539679},
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'}
   }
